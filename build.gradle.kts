e5450534c2d086e747f2f337aea449a9a815b898
murphy2505 <mervynhendriks@gmail.com>
2025-10-22 09:59:25 +0200

    Refactor plugin management in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,12 @@
-plugins { id("com.android.application") version "8.6.0" apply false
-          id("org.jetbrains.kotlin.android") version "2.0.0" apply false }
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+plugins {
+    id("com.android.application") version "8.6.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+}
+
