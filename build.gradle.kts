b6c4dc0bafeb03f837a3bcf30a1a84dfae002c6c
murphy2505 <mervynhendriks@gmail.com>
2025-10-22 10:05:42 +0200

    Remove pluginManagement block from build.gradle.kts
    Removed plugin management section from build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,4 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
 plugins {
     id("com.android.application") version "8.6.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
 }
-
